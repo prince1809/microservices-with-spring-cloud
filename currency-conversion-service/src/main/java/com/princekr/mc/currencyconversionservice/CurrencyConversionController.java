fd80bdfd2f40a0ce15c24c8c54a6cd0fb2e64d1b
prince <prince18pat@gmail.com>
2020-03-07 17:29:11 +0900

    add ribbon and feign client

--- currency-conversion-service/src/main/java/com/princekr/mc/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/princekr/mc/currencyconversionservice/CurrencyConversionController.java
@@ -1,18 +1,51 @@
 package com.princekr.mc.currencyconversionservice;
 
+import java.beans.PropertyChangeListenerProxy;
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
 
-	@GetMapping("currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+	@Autowired
+	private CurrencyExchangeServiceProxy proxy;
+
+	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to,
 			@PathVariable BigDecimal quantity) {
-		return new CurrencyConversionBean(100L, from, to, quantity, quantity, quantity, 0);
+
+		Map<String, String> uriVariables = new HashMap<String, String>();
+
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
+
+		ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity(
+				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class,
+				uriVariables);
+
+		CurrencyConversionBean response = responseEntity.getBody();
+
+		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
+				quantity.multiply(response.getConversionMultiple()), response.getPort());
+
+	}
+
+	@GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+
+		CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
+
+		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
+				quantity.multiply(response.getConversionMultiple()), response.getPort());
 
 	}
 
--- currency-exchange-service/src/main/resources/data.sql
+++ currency-exchange-service/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO exchange_value (id,currency_from,currency_to,conversion_multiple, port) values(1001, 'INR', 'USD', 72, 0);
-INSERT INTO exchange_value (id,currency_from,currency_to,conversion_multiple, port) values(1002, 'INR', 'JPY', 0.68, 0);
-INSERT INTO exchange_value (id,currency_from,currency_to,conversion_multiple, port) values(1003, 'INR', 'EUR', 75, 0);
+INSERT INTO exchange_value (id,currency_from,currency_to,conversion_multiple, port) values(1001,'USD', 'INR', 72, 0);
+INSERT INTO exchange_value (id,currency_from,currency_to,conversion_multiple, port) values(1002, 'JPY', 'INR', 0.68, 0);
+INSERT INTO exchange_value (id,currency_from,currency_to,conversion_multiple, port) values(1003, 'EUR', 'INR', 75, 0);
