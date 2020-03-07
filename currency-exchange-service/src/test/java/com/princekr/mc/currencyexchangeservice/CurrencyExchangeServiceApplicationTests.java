01c6f3bdf5956e32e38781a6a665e1762041e273
prince <prince18pat@gmail.com>
2020-03-07 14:03:32 +0900

    add currency conversion service and currency exchange service

---
+++ currency-exchange-service/src/test/java/com/princekr/mc/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.princekr.mc.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
